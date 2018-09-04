37e6555132b8b17c23b30056429d8ef5e43323c8
feifuzeng <feifz@tydic.com>
2018-09-04 16:48:55 +0800

    springcloud config bus init

--- config-client/src/main/java/com/feifz/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/feifz/configclient/ConfigClientApplication.java
@@ -3,11 +3,17 @@ package com.feifz.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -16,11 +22,9 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
-    @Value("${democonfigclient.message}")
-    String message;
 
     @RequestMapping(value = "/hi")
     public String hi(){
-        return message;
+        return foo;
     }
 }
